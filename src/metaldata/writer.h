36c216b64a28a2edd1f56765275664381f9ca243
Diego Argueta <dargueta@users.noreply.github.com>
2023-10-01 09:37:00 -0700

    Add Writer

---
+++ src/metaldata/writer.h
@@ -0,0 +1,83 @@
+/**
+ * An unbuffered output stream abstraction.
+ *
+ * @file writer.h
+ */
+
+#ifndef INCLUDE_METALDATA_WRITER_H_
+#define INCLUDE_METALDATA_WRITER_H_
+
+#include "annotations.h"
+#include "metaldata.h"
+#include <stdbool.h>
+
+struct MDLWriter_;
+typedef struct MDLWriter_ MDLWriter;
+
+typedef int (*mdl_writer_putc_fptr)(MDLWriter *writer, int chr) MDL_REENTRANT_MARKER;
+typedef void (*mdl_writer_close_fptr)(MDLWriter *writer) MDL_REENTRANT_MARKER;
+
+/**
+ * A byte-oriented writer that abstracts away details of the source.
+ *
+ * @warning The struct is declared in the header only to allow users to allocate it on the
+ *          stack. Do not modify it directly.
+ */
+struct MDLWriter_
+{
+    MDLState *ds;
+    mdl_writer_putc_fptr putc_ptr;
+    mdl_writer_close_fptr close_ptr;
+    void *udata;
+    char *output_buffer;
+    size_t output_size;
+    size_t buffer_position;
+    bool was_allocated;
+};
+
+MDL_API
+MDL_ANNOTN__NONNULL_ARGS(1, 2)
+MDL_ANNOTN__NODISCARD
+MDLWriter *mdl_writer_new(MDLState *ds, mdl_writer_putc_fptr putc_ptr,
+                          mdl_writer_close_fptr close_ptr, void *udata);
+
+MDL_API
+MDL_ANNOTN__NONNULL
+MDL_ANNOTN__NODISCARD
+MDL_ANNOTN__ACCESS_SIZED(read_only, 2, 3)
+MDLWriter *mdl_writer_newfrombuffer(MDLState *MDL_ANNOTN__RESTRICT ds,
+                                    void *MDL_ANNOTN__RESTRICT buffer, size_t size);
+
+MDL_API
+MDL_ANNOTN__NONNULL_ARGS(1, 2)
+void mdl_writer_init(MDLState *MDL_ANNOTN__RESTRICT ds,
+                     MDLWriter *MDL_ANNOTN__RESTRICT writer,
+                     mdl_writer_putc_fptr putc_ptr, mdl_writer_close_fptr close_ptr,
+                     void *udata);
+
+MDL_API
+MDL_ANNOTN__NONNULL
+MDL_ANNOTN__ACCESS_SIZED(write_only, 3, 4)
+void mdl_writer_initfrombuffer(MDLState *MDL_ANNOTN__RESTRICT ds,
+                               MDLWriter *MDL_ANNOTN__RESTRICT writer,
+                               void *MDL_ANNOTN__RESTRICT buffer, size_t size);
+
+MDL_API
+MDL_ANNOTN__NONNULL
+void *mdl_writer_getudata(const MDLWriter *writer);
+
+MDL_API
+MDL_ANNOTN__NONNULL
+int mdl_writer_close(MDLWriter *writer);
+
+MDL_API
+MDL_ANNOTN__NONNULL
+int mdl_writer_putc(MDLWriter *writer, int chr);
+
+MDL_API
+MDL_ANNOTN__NONNULL
+MDL_ANNOTN__ACCESS_SIZED(read_only, 2, 3)
+size_t mdl_writer_write(MDLWriter *MDL_ANNOTN__RESTRICT writer, const void *data,
+                        size_t size);
+
+#endif /* INCLUDE_METALDATA_WRITER_H_ */
---
+++ src/writer.c
@@ -0,0 +1,96 @@
+#include "metaldata/writer.h"
+#include "metaldata/metaldata.h"
+
+static int memory_putc(MDLWriter *writer, int chr);
+
+MDLWriter *mdl_writer_new(MDLState *ds, mdl_writer_putc_fptr putc_ptr,
+                          mdl_writer_close_fptr close_ptr, void *udata)
+{
+    MDLWriter *writer;
+    writer = mdl_malloc(ds, sizeof(*writer));
+    if (writer == NULL)
+        return NULL;
+
+    mdl_writer_init(ds, writer, putc_ptr, close_ptr, udata);
+    return writer;
+}
+
+MDLWriter *mdl_writer_newfrombuffer(MDLState *MDL_ANNOTN__RESTRICT ds,
+                                    void *MDL_ANNOTN__RESTRICT buffer, size_t size)
+{
+    MDLWriter *writer = mdl_writer_new(ds, memory_putc, NULL, NULL);
+    if (writer == NULL)
+        return NULL;
+
+    mdl_writer_initfrombuffer(ds, writer, buffer, size);
+    return writer;
+}
+
+void mdl_writer_init(MDLState *MDL_ANNOTN__RESTRICT ds,
+                     MDLWriter *MDL_ANNOTN__RESTRICT writer,
+                     mdl_writer_putc_fptr putc_ptr, mdl_writer_close_fptr close_ptr,
+                     void *udata)
+{
+    writer->ds = ds;
+    writer->putc_ptr = putc_ptr;
+    writer->close_ptr = close_ptr;
+    writer->udata = udata;
+    writer->output_buffer = NULL;
+    writer->output_size = 0;
+    writer->buffer_position = 0;
+    writer->was_allocated = false;
+}
+
+void mdl_writer_initfrombuffer(MDLState *MDL_ANNOTN__RESTRICT ds,
+                               MDLWriter *MDL_ANNOTN__RESTRICT writer,
+                               void *MDL_ANNOTN__RESTRICT buffer, size_t size)
+{
+    mdl_writer_init(ds, writer, memory_putc, NULL, NULL);
+    writer->output_buffer = buffer;
+    writer->output_size = size;
+}
+
+void *mdl_writer_getudata(const MDLWriter *writer)
+{
+    return writer->udata;
+}
+
+int mdl_writer_close(MDLWriter *writer)
+{
+    if (writer->close_ptr)
+        writer->close_ptr(writer);
+
+    if (writer->was_allocated)
+        mdl_free(writer->ds, writer, sizeof(*writer));
+    return 0;
+}
+
+int mdl_writer_putc(MDLWriter *writer, int chr)
+{
+    return writer->putc_ptr(writer, chr);
+}
+
+size_t mdl_writer_write(MDLWriter *MDL_ANNOTN__RESTRICT writer, const void *data,
+                        size_t size)
+{
+    size_t n_written;
+    const char *current_byte = data;
+
+    for (n_written = 0; n_written < size; n_written++)
+    {
+        int result = writer->putc_ptr(writer, *current_byte);
+        if (result != MDL_OK)
+            return n_written;
+        ++current_byte;
+    }
+    return size;
+}
+
+static int memory_putc(MDLWriter *writer, int chr)
+{
+    if (writer->buffer_position == writer->output_size)
+        return MDL_ERROR_FULL;
+
+    writer->output_buffer[writer->buffer_position++] = (char)chr;
+    return 0;
+}

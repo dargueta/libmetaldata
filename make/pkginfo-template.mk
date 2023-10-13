3b863cdc8a110ced23276156fcea466979406d47
Diego Argueta <dargueta@users.noreply.github.com>
2023-10-12 21:50:52 -0700

    Fix wrong include file path in pkginfo file

--- make/pkginfo-template.mk
+++ make/pkginfo-template.mk
@@ -3,6 +3,6 @@ Name: $(LIB_NAME_STEM)
 Description: Data structures and abstractions for low-level software
 URL: https://github.com/dargueta/libmetaldata
 Version: $(VERSION_FULL)
-Cflags: $(PUBLIC_COMPILE_FLAGS) -I$(INSTALL_TARGET_INCLUDE)
-Libs: -L$(INSTALL_TARGET_LIB) -lmetaldata
+Cflags: $(strip $(PUBLIC_COMPILE_FLAGS) -I$(PREFIX)/include)
+Libs: -L$(PREFIX)/lib -lmetaldata
 endef
